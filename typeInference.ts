73b8082fd5b4fc6c060a3acb06958becef540e1a
Adi4512 <adityasharma4510@gmail.com>
2024-10-03 00:41:43 +0530

    type inference

---
+++ typeInference.ts
@@ -0,0 +1,14 @@
+
+let tech='MERN';
+// tech=15; this will throw error as above it has inferenced as string;
+console.log(tech + ' and the type is ' + typeof tech);
+
+let exp=2;
+console.log(exp + ' and the type is ' + typeof exp);
+// exp='2';  this will throw error as above it has inferenced as number;
+
+let ishard=true
+console.log(ishard + ' and the type is ' + typeof ishard);
+
+// ishard='true';  this will throw error as above it has inferenced as boolean.
+
